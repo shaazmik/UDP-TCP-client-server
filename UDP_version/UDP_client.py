3fcd70b95870bc0a9cd29dbae670e4993bc685f1
Daniil Home-PC <paramonov.dr@phystech.edu>
2022-06-27 20:09:56 +0300

    start version UDP client-server

--- UDP_version/UDP_client.py
+++ UDP_version/UDP_client.py
@@ -1,7 +1,14 @@
 from socket import *
-server_name = 'hostname'
+
+server_name = '192.168.31.201'
 server_port = 1337 
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = input('Input lowercase sentence:')
+message = (input('Input lowercase sentence:'))
+clientSocket.sendto(message.encode(),(server_name, server_port))
+
+returned_message, server_address = clientSocket.recvfrom(2048)
+print(returned_message)
+
+clientSocket.close()
---
+++ UDP_version/UDP_server.py
@@ -0,0 +1,13 @@
+from socket import *
+
+server_port = 1337
+server_socket = socket(AF_INET, SOCK_DGRAM)
+
+server_socket.bind(('', server_port))
+
+print("The server is ready to receive")
+
+while 1:
+    message, client_address = server_socket.recvfrom(2048)
+    modified_message = message.upper()
+    server_socket.sendto(modified_message, client_address)
